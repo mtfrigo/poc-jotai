472d43c97f73c727746186ca50aaa43cff81ef80
Matheus Tavares Frigo <matheus_frigo@sicredi.com.br>
2025-03-11 12:09:59 -0300

    feat: http client and axios adpater

--- src/infra/http/axios-client.adapter.ts
+++ src/infra/http/axios-client.adapter.ts
@@ -1,31 +1,27 @@
-// import axios, { AxiosError, AxiosInstance } from 'axios'
-// import { IHttpClient, HttpRequest } from '../contracts/http-contracts'
+import axios, { AxiosError, AxiosInstance } from 'axios'
+import { HttpClient, HttpRequest } from './http-client.contracts'
 
-// const baseURL = 'http://localhost:3000'
+const baseURL = 'http://localhost:3000'
 
-// export class HttpClient implements IHttpClient {
-// 	constructor(private api: AxiosInstance) {}
+export class AxiosHttpClient implements HttpClient {
+	constructor(private api: AxiosInstance = axios) {}
 
-// 	static create(): HttpClient {
-// 		return new HttpClient(axios)
-// 	}
-
-// 	async sendRequest<TResponse, TBody>(props: HttpRequest<TBody>): Promise<TResponse> {
-// 		const { endpoint, method, body, headers, params } = props
-// 		try {
-// 			const { data } = await this.api.request<TResponse>({
-// 				method,
-// 				headers,
-// 				data: body,
-// 				url: `${baseURL}${endpoint}`,
-// 				params,
-// 			})
-// 			return data
-// 		} catch (er) {
-// 			const error = er as AxiosError
-// 			const status = error.response?.status || 500
-// 			const message = error.response?.data || error.message
-// 			throw new Error(`Request failed with status ${status}: ${message}`)
-// 		}
-// 	}
-// }
+	async request<TResponse, TBody>(props: HttpRequest<TBody>): Promise<TResponse> {
+		const { endpoint, method, body, headers, params } = props
+		try {
+			const { data } = await this.api.request<TResponse>({
+				method,
+				headers,
+				data: body,
+				url: `${baseURL}${endpoint}`,
+				params,
+			})
+			return data
+		} catch (er) {
+			const error = er as AxiosError
+			const status = error.response?.status || 500
+			const message = error.response?.data || error.message
+			throw new Error(`Request failed with status ${status}: ${message}`)
+		}
+	}
+}
---
+++ src/infra/http/http-client.contracts.ts
@@ -0,0 +1,21 @@
+export enum HttpMethod {
+    GET = "get",
+    POST = "post",
+    PUT = "put",
+    DELETE = "delete",
+}
+  
+export type HttpRequest<TBody = unknown> = {
+    endpoint: string;
+    method: HttpMethod;
+    body?: TBody;
+    headers?: Record<string, string | number>;
+    params?: Record<string, string | number>;
+};
+
+export interface HttpClient {
+    request: <TResponse, TBody = unknown>(
+        request: HttpRequest<TBody>
+    ) => Promise<TResponse>;
+}
+  
